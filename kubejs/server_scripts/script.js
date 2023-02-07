732d75d435713b178695c96d35451807a52d8884
MignonPetitXelow <xeloxlegrande@gmail.com>
2023-02-07 19:37:19 +0000

    Adding funny scripts

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -1,20 +1,12 @@
-// priority: 0
-
-settings.logAddedRecipes = true
-settings.logRemovedRecipes = true
-settings.logSkippedRecipes = false
-settings.logErroringRecipes = true
-
-console.info('Hello, World! (You will see this line every time server resources reload)')
+onEvent('block.loot_tables', event => {
+	event.addBlock(/minecraft:.*_leaves/, table => {
+		table.addPool(pool => {
+			pool.survivesExplosion()
+			pool.addItem('minecraft:stick', 1, [1, 3])
+		})
+	})
+});
 
 onEvent('recipes', event => {
-	// Change recipes here
-})
-
-onEvent('item.tags', event => {
-	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
-	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
-
-	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
-	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
-})
+	event.remove({output: 'minecraft:stick'})
+});
