5c30210751b74590cbad0407bf510db3c4356950
MignonPetitXelow <xeloxlegrande@gmail.com>
2023-02-07 20:30:57 +0000

    Test

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -1,6 +1,6 @@
 onEvent('block.loot_tables', event => {
-	event.addBlock(/minecraft:.*_leaves/, table => {
-		table.addPool(pool => {
+	event.addBlock(/minecraft:.*_leaves/, x => {
+		x.addPool(pool => {
 			pool.survivesExplosion()
 			pool.addItem('minecraft:stick', 1, [1, 3])
 		})
@@ -8,5 +8,12 @@ onEvent('block.loot_tables', event => {
 });
 
 onEvent('recipes', event => {
-	event.remove({output: 'minecraft:stick'})
+	event.remove({output: 'minecraft:stick'});
+
+	event.shapeless('3x farmerdelights:rope', [
+		'SD',
+	], {
+		S: Item.of('farmerdelights:flint_knife').ignoreNBT(),
+		D: 'minecraft:stick'
+	}).damageIngredient(1).keepIngredient('farmerdelights:flint_knife')
 });
